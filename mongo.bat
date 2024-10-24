2e7ad698285d341652953fa56a78fc4d73e6fe5e
Simon S <107688168+jit-compiler@users.noreply.github.com>
2024-10-24 17:32:27 +0200

    added some basic invite commands as well as leave support

---
+++ mongo.bat
@@ -0,0 +1,2 @@
+cd C:\Users\23simsvo\mongodb\bin
+.\mongod --config C:/Users/23simsvo/MongoDB/mongod.cfg
---
+++ src/commands/invites/bonus/add.ts
@@ -0,0 +1,29 @@
+import { ChatInputCommandInteraction } from "discord.js";
+import { cs } from "../../../utils/console/customConsole";
+import { db } from "../../../utils/db/db";
+
+export default async function (interaction: ChatInputCommandInteraction) {
+    try {
+        const user = interaction.options.getUser("user", true);
+        const count = interaction.options.getInteger("count", true);
+
+        await interaction.deferReply({ ephemeral: true });
+
+        await db.invites.userInvites.addBonus(interaction.guildId!, user.id, count);
+
+        await interaction.followUp({
+            content: `Successfully added ${count} bonus invites to ${user.tag}`,
+        });
+    } catch (error: unknown) {
+        cs.error("Error while handling bonus-invites remove command: " + error);
+
+        try {
+            await interaction.followUp({
+                content: "An error occurred while adding bonus invites to the user",
+                ephemeral: true,
+            });
+        } catch (error: unknown) {
+            cs.error("Error while sending an error message for bonus-invites add command: " + error);
+        }
+    }
+}
