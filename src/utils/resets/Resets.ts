5f153ccc808e0b3b08bb7b14252879f4a635591b
Simon <svobodasimon1@gmail.com>
2024-11-16 18:43:05 +0100

    added sharding support for centralized reset system, cleaned up the code a little

---
+++ src/utils/resets/Resets.ts
@@ -0,0 +1,29 @@
+import { cs } from "../console/customConsole";
+import { db } from "../db/db";
+
+export class Resets {
+  static async resetMonthly() {
+    await db.leaderboards.resetTimedLeaderboard("monthly");
+    await db.invites.userInvites.resetTimedInvites("monthly");
+    await db.reactionRoles.resetUses();
+    await db.resets.setLastMonthlyReset();
+    
+    cs.log("Monthly tasks reset successfully.");
+  }
+
+  static async resetWeekly() {
+    await db.leaderboards.resetTimedLeaderboard("weekly");
+    await db.invites.userInvites.resetTimedInvites("weekly");
+    await db.resets.setLastWeeklyReset();
+
+    cs.log("Weekly tasks reset successfully.");
+  }
+
+  static async resetDaily() {
+    await db.leaderboards.resetTimedLeaderboard("daily");
+    await db.invites.userInvites.resetTimedInvites("daily");
+    await db.resets.setLastDailyReset();
+
+    cs.log("Daily tasks reset successfully.");
+  }
+}
