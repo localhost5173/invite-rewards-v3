365035f9ebe4d6f80f1f82bb4160959b81ee25ad
Simon <svobodasimon1@gmail.com>
2024-12-09 21:07:44 +0100

    added type tracking to usage

--- src/utils/db/categories/usage.ts
+++ src/utils/db/categories/usage.ts
@@ -1,6 +1,9 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import CommandLogModel from "../models/commandLogModel.js";
-import UsageModel, { UsageCommands, UsageEvents } from "../models/usageModel.js";
+import UsageModel, {
+  UsageCommands,
+  UsageEvents,
+} from "../models/usageModel.js";
 
 export class usage {
   static async incrementUses(
@@ -11,9 +14,14 @@ export class usage {
     const month = new Date().getMonth() + 1;
     const year = new Date().getFullYear();
 
+    // Determine the type based on the feature
+    const type = Object.values(UsageCommands).includes(feature as UsageCommands)
+      ? "command"
+      : "event";
+
     await UsageModel.updateOne(
       { guildId, feature, day, month, year },
-      { $inc: { count: 1 } },
+      { $inc: { count: 1 }, $set: { type } },
       { upsert: true }
     );
   }
@@ -60,7 +68,7 @@ function formatOption(option: any): string {
       " " +
       option.options
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        .map((subOption : any) => formatOption(subOption))
+        .map((subOption: any) => formatOption(subOption))
         .join(" ");
   }
 
