29b2f7630dd511964f12242d6320d2bb7a5b8006
Simon <svobodasimon1@gmail.com>
2024-12-11 16:49:03 +0100

    made invites command available to common folks

--- src/commands/invites/invites.ts
+++ src/commands/invites/invites.ts
@@ -1,5 +1,9 @@
-import type { CommandData, SlashCommandProps, CommandOptions } from "commandkit";
-import { ApplicationCommandOptionType } from "discord.js";
+import type {
+  CommandData,
+  SlashCommandProps,
+  CommandOptions,
+} from "commandkit";
+import { ApplicationCommandOptionType, PermissionFlagsBits, PermissionsBitField } from "discord.js";
 import { db } from "../../utils/db/db.js";
 import { devMode } from "../../index.js";
 import { Embeds } from "../../utils/embeds/embeds.js";
@@ -7,67 +11,86 @@ import { Helpers } from "../../utils/helpers/helpers.js";
 import { UsageCommands } from "../../utils/db/models/usageModel.js";
 
 export const data: CommandData = {
-    name: "invites",
-    description: "View the invites of a user",
-    options: [
-        {
-            name: "user",
-            description: "The user to view the invites of",
-            type: ApplicationCommandOptionType.User,
-            required: false,
-        },
-        {
-            name: "breakdown",
-            description: "View the breakdown of the invites",
-            type: ApplicationCommandOptionType.Boolean,
-            required: false,
-        }
-    ]
+  name: "invites",
+  description: "View the invites of a user",
+  options: [
+    {
+      name: "user",
+      description: "The user to view the invites of",
+      type: ApplicationCommandOptionType.User,
+      required: false,
+    },
+    {
+      name: "breakdown",
+      description: "View the breakdown of the invites",
+      type: ApplicationCommandOptionType.Boolean,
+      required: false,
+    },
+  ],
 };
 
 export async function run({ interaction }: SlashCommandProps) {
-    try {
-        const user = interaction.options.getUser("user") ?? interaction.user;
-        const breakdown = interaction.options.getBoolean("breakdown") ?? false;
-        const guildId = interaction.guildId!;
+  try {
+    const user = interaction.options.getUser("user") ?? interaction.user;
+    const breakdown = interaction.options.getBoolean("breakdown") ?? false;
+    const guildId = interaction.guildId!;
 
-        const invites = await db.invites.userInvites.getInvites(guildId, user.id);
-
-        const real = invites?.real ?? 0;
-        const bonus = invites?.bonus ?? 0;
-        const fake = invites?.fake ?? 0;
-        const unverified = invites?.unverified ?? 0;
-
-        db.usage.incrementUses(guildId, UsageCommands.InviteCountView);
-
-        if (breakdown) {
-            const embed = await Embeds.createEmbed(guildId, "invites.breakdown", {
-                user: user.tag,
-                real: real.toString(),
-                bonus: bonus.toString(),
-                fake: fake.toString(),
-                unverified: unverified.toString(),
-            });
-            await interaction.reply({ embeds: [embed], ephemeral: true });
-        } else {
-            const totalInvites = real + bonus;
-            const embed = await Embeds.createEmbed(guildId, "invites.total", {
-                user: user.tag,
-                totalInvites: totalInvites.toString(),
-            });
-            await interaction.reply({ embeds: [embed], ephemeral: true });
-        }
-    } catch (error: unknown) {
-        console.error("Error while getting invites: " + error);
-        await Helpers.trySendCommandError(interaction);
+    // Only allow users with Manage Guild permission to view the invites of other users
+    const permissions = interaction.member?.permissions as PermissionsBitField;
+    console.log("permissions", permissions);
+    if (
+      (user !== interaction.user || breakdown) &&
+      (!permissions || !permissions.has(PermissionFlagsBits.ManageGuild))
+    ) {
+      await interaction.reply({
+        embeds: [
+            await Embeds.createEmbed(
+                guildId,
+                "invites.noPermissions"
+            ),
+        ],
+        ephemeral: true,
+      });
+      return;
     }
+
+    const invites = await db.invites.userInvites.getInvites(guildId, user.id);
+
+    const real = invites?.real ?? 0;
+    const bonus = invites?.bonus ?? 0;
+    const fake = invites?.fake ?? 0;
+    const unverified = invites?.unverified ?? 0;
+
+    db.usage.incrementUses(guildId, UsageCommands.InviteCountView);
+
+    if (breakdown) {
+      const embed = await Embeds.createEmbed(guildId, "invites.breakdown", {
+        user: user.tag,
+        real: real.toString(),
+        bonus: bonus.toString(),
+        fake: fake.toString(),
+        unverified: unverified.toString(),
+      });
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+    } else {
+      const totalInvites = real + bonus;
+      const embed = await Embeds.createEmbed(guildId, "invites.total", {
+        user: user.tag,
+        totalInvites: totalInvites.toString(),
+      });
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+    }
+  } catch (error: unknown) {
+    console.error("Error while getting invites: " + error);
+    await Helpers.trySendCommandError(interaction);
+  }
 }
 
 export const options: CommandOptions = {
-    devOnly: devMode,
-    userPermissions: ["ManageGuild"],
-    botPermissions: ["SendMessages", "EmbedLinks"],
-    deleted: false,
-    onlyGuild: true,
-    voteLocked: true,
+  devOnly: devMode,
+  userPermissions: [],
+  botPermissions: ["SendMessages", "EmbedLinks"],
+  deleted: false,
+  onlyGuild: true,
+  voteLocked: true,
 };
