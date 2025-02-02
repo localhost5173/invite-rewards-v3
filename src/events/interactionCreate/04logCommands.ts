14d37c912c41a2577634f4143ab65b7789c17ba9
Simon <svobodasimon1@gmail.com>
2025-02-02 22:10:39 +0100

    hotfix for sending ads

--- src/events/interactionCreate/04logCommands.ts
+++ src/events/interactionCreate/04logCommands.ts
@@ -4,5 +4,5 @@ import { db } from "../../utils/db/db.js";
 export default async function (interaction: Interaction) {
   if (!interaction.isCommand()) return;
   if (!interaction.isChatInputCommand()) return;
-  db.usage.logCommand(interaction);
+  await db.usage.logCommand(interaction);
 }
